19e970583100a5cd47dc9d085b4acf218bccb12e
sch93nz <sch93nz@hotmail.com>
2016-03-13 13:32:00 +1300

    python
    a script to make the data look nice and clean.

---
+++ GPSParser/GPStest/StripAndClean.py
@@ -0,0 +1,19 @@
+class degree:
+	Time = 00.00
+	X = 00.00
+	Y = 00.00
+	Z = 00.00
+
+class output:
+	time = 00.00
+	X = 00.00
+	Y = 00.00
+	Z = 00.00
+	
+file = 0
+
+def main():
+	global file
+	file = open("")
+	load()
+	
