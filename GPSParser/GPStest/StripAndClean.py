04424deea3230432780a778bf1f2745006cb599d
_Casper_ <sch93nz@hotmail.com>
2016-03-15 09:44:11 +1300

    working on python code
    so working on the python code almost there just got to write it two two
    files as a output and we should be done with it.

--- GPSParser/GPStest/StripAndClean.py
+++ GPSParser/GPStest/StripAndClean.py
@@ -12,8 +12,53 @@ class output:
 	
 file = 0
 
+degr = []
+data = [] 
 def main():
 	global file
-	file = open("")
+	file = open("D3431.TXT")
 	load()
+	print "output"
+	print degr
+	print data
 	
+	
+	
+def load ():
+	for line in file:
+		if "degress" in line:
+	#deal with degree
+			dealDegree(line)
+		else :
+			dealData(line)
+	
+	
+
+def dealData(str):
+	points = str.split(",")
+	print points
+	if len(points)>2:
+		p = output
+		p.time =points[0]
+		p.X=points[1]
+		p.Y=points[2]
+		p.Z=points[3]
+		data.append(p)
+	else :
+		print "nothing"
+	
+def dealDegree(str):
+	points = str.split(",")
+	print points
+	if len(points)>2:
+		p = degree
+		p.time =points[0]
+		p.X=points[1]
+		p.Y=points[2]
+		p.Z=points[3]
+		degr.append(p)
+	else :
+		print "nothing"
+
+		
+main()
